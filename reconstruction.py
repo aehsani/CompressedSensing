9e8c17d0e4ff145ebf5757098714ef9e0af47b16
Anis Ehsani <anis.ehsani@gmail.com>
2020-07-21 11:54:03 -0400

    reconstruction edits

--- reconstruction.py
+++ reconstruction.py
@@ -6,41 +6,49 @@ from scipy import sparse
 import gurobipy as gp
 
 class SmoothGreedy:
-    def __init__(self, A, Y, k, data=None, eps = 0.1):
-        self.k = k
-        self.d, self.points = self.data.shape
-        self.current_objective_val = 0
+    def __init__(self, A, Y, data=None, k=12, eps=0.01):
         self.A = A
         self.Y = Y
         self.eps = eps
         self.data = data
-        self.reconstruction
 
-        # self.reconstruction = np.zeros_like(A)
+        self.n = Y.shape[0]
+        self.d = A.shape[0]
+        self.k = min(k, self.d)
+
+        self.reconstruction = np.zeros((self.n, self.d))
+        self.current_objective_val = 0
+        self.compute_reconstruction()
+        
+
     def evaluate(self):
-        if not data:
+        if data is None:
             raise ValueError("No data to evaluate error")
+        unscaled = np.linalg.norm(self.data - self.reconstruction, ord='fro')
+        return unscaled/np.sqrt(self.n)
     
     def compute_reconstruction(self):
-        for i in range(self.points):
-            self.recontruction[:,i] = self.compute_estimate_vector(i)
+        for i in range(self.n):
+            if i % 50 == 0:
+                print("Doing vector: ", i)
+            entries, indices = self.compute_best_indices(i)
+            self.reconstruction[i,indices] = entries
 
     def compute_best_indices(self, i):
         current_indices = set()
-        remaining = set(range(d))
-        measurement = y[:,i]
+        remaining = set(range(self.d))
+        measurement = Y[i,:]
         l0 = 1/2*np.inner(measurement, measurement)
         current_obj_val = 0
         best_x = None
 
-        for _ in range(k):
-            r = len(remaining)
+        for _ in range(self.k):
             g = dict()
             objective = dict()
             for j in remaining:
                 current_indices.add(j)
 
-                best_x, loss = self.find_best_x(current_indices)
+                best_x, loss = self.find_best_x(current_indices, measurement)
                 objective[j] = l0 - loss
                 g[j] = objective[j] - current_obj_val
 
@@ -49,15 +57,17 @@ class SmoothGreedy:
             remaining.remove(best_j)
             current_indices.add(best_j)
             current_obj_val = objective[best_j]
-        return current_indices
-        
+
+        best_x, _ = self.find_best_x(current_indices, measurement)
+        return best_x, sorted(current_indices)
 
                 
     def choose_index(self, g):
-        g_vec = [value for key,value in sorted(attributes.items())]
-        p_vec = self.get_p_quadratic(g_vec)
-        chosen = np.random.choice(list(range(len(g_vec))), p=p_vec)
-        return chosen
+        g_items = sorted(g.items())
+        g_vec = np.array([value for key,value in g_items])
+        p_vec = self.get_p_entr(g_vec)
+        chosen = np.random.choice(len(g_vec), p=p_vec)
+        return g_items[chosen][0]
 
     def get_p_quadratic(self, g_vec):
         N = len(g_vec)
@@ -86,12 +96,14 @@ class SmoothGreedy:
         p_vec = p_vec/p_vec.sum()
         return p_vec
                 
-    def find_best_x(self, indices):
+    def find_best_x(self, indices, measurement):
         indices = sorted(indices)
-        new_A = A[:indices]
-        new_y = y[:i]
-        x_dict = np.linalg.lstsq(new_A, new_y)
+        new_A = self.A[indices,:]
+        x_dict = np.linalg.lstsq(new_A.transpose(), measurement.transpose(), rcond=None)
         return x_dict[0], x_dict[1].sum()/2
+
+def compute_avg_nonzero(arr):
+    pass
 
 if __name__ == "__main__":
     data = sparse.load_npz('data/data_1.npz')
@@ -105,6 +117,9 @@ if __name__ == "__main__":
     print("Y")
     print(Y.shape)
 
-    sg = SmoothGreedy
+    sg = SmoothGreedy(A, Y, data, k=20)
+    rmse = sg.evaluate()
+    print("RMSE: ", rmse)
+    np.savetxt("data/recovered_1", sg.reconstruction, delimiter=',')
 
     
