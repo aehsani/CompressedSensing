659f70e65b3aac157001c2951a78d959b659dbb3
Marwa El Halabi <marwash25@gmail.com>
2022-02-08 15:17:17 -0500

    committing old changes

--- reconstruction-M.py
+++ reconstruction-M.py
@@ -12,10 +12,10 @@ class SmoothGreedy:
         self.eps = eps
         self.data = data
 
-        self.n = Y.shape[1] # number of vectors
-        self.d = A.shape[1] # dimension of vectors
-        self.m = A.shape[0] # dimension of measurements
-        self.k = min(k, self.d) # cardinality constraint
+        self.n = Y.shape[1]  # number of vectors # M: this should be Y.shape[0] given current input in main
+        self.d = A.shape[1]  # dimension of vectors # M: this should be A.shape[0] given current input in main
+        self.m = A.shape[0]  # dimension of measurements # M: this should be A.shape[1] given current input in main
+        self.k = min(k, self.d)  # cardinality constraint
 
         self.reconstruction = np.zeros((self.d, self.n))
         self.current_objective_val = 0
@@ -29,9 +29,9 @@ class SmoothGreedy:
         return unscaled/np.sqrt(self.n)
     
     def compute_reconstruction(self):
-        for i in range(self.n):
-            #if i % 50 == 0:
-            #    print("Doing vector: ", i)
+        for i in range(self.n):  # TODO: do this in parallel?
+            if i % 50 == 0:
+                print("Doing vector: ", i)
             entries, indices = self.compute_best_indices(i)
             self.reconstruction[indices,i] = entries
 
@@ -67,12 +67,11 @@ class SmoothGreedy:
         pseudo_inv = np.zeros((self.k,self.m))
         A_sub = np.zeros((self.m,self.k))
         remaining = set(range(self.d))
-        measurement = Y[:,i]
+        measurement = Y[:, i]  # M: this should be Y[i, :]
         l0 = 1/2*np.inner(measurement, measurement)
         current_obj_val = 0
         best_x = None
         chosen = []
-        g_list = []
 
         for _ in range(self.k):
             g = dict()
@@ -80,11 +79,10 @@ class SmoothGreedy:
 
             for j in remaining:
                 new_A_sub, new_pseudo = self.update_matrices(j, chosen, A_sub, pseudo_inv)
-                best_x, loss = self.r1_find_best_x(measurement, new_A_sub, new_pseudo)
+                best_x, loss = self.r1_find_best_x(j, current_indices, measurement, A_sub, pseudo_inv)
                 objective[j] = l0 - loss
                 g[j] = objective[j] - current_obj_val
 
-            g_list.append(g)
             best_j = self.choose_index(g)
             remaining.remove(best_j)
             current_indices.add(best_j)
@@ -94,71 +92,18 @@ class SmoothGreedy:
         return best_x, sorted(current_indices)
 
     def update_matrices(self, j, chosen, A_sub, pseudo_inv):
-        # i is next index to be filled in A_sub
-        # j is the index chosen from the parent iteration algorithm
-        # so instead of progressively growing the matrix A_S and its pseudoinverse
-        # this is starting with the matrices of final size (A_S m x k, and its pseudo-inv k x m)
-        # with zeros in the part not updated yet (index i to end).
-        # But why do this? it's less efficient than progressively growing the matrices
-        i = len(chosen)
-        new_A_sub = A_sub.copy()
-        new_pseudo_inv = pseudo_inv.copy()
-        # TODO change this later to make this faster
-        new_A_sub[:, i] = self.A[:, j]
         
-        c = self.A[:j]  # this should be self.A[:, j]
-        d = np.zeros((1,self.k))
-        d[0,i] = 1
-        # it is more efficient to do the computation of w and m the way I do in the Matlab code I sent you
-        # i.e., compute first V = pseudo_inv @ c, then w = c - A @ V, similarly for m
-        w = (np.identity(self.m) - A_sub @ pseudo_inv) @ c
-        m = (np.identity(self.k) - pseudo_inv @ A_sub).T @ d
+        return new_A_sub, new_pseudo_inv
 
-        v = pseudo_inv @ c
-        beta = (d.T @ pseudo_inv @ c)[0,0] + 1
-        n = pseudo_inv.T @ d
+    def r1_find_best_x(self, j, indices, measurement, A_sub, pseudo_inv):
+        q = len(indices) + 1
 
-        w_sq = np.linalg.norm(w, 2)**2
-        m_sq = np.linalg.norm(m, 2)**2
-        v_sq = np.linalg.norm(v, 2)**2
-        n_sq = np.linalg.norm(n, 2)**2
+        sorted_indices = sorted(indices)
+        pos = sorted_indices.index(j)
 
-        threshold = 10e-8
-        if w_sq > threshold and m_sq > threshold:
-            G = -v @ w.T/w_sq**2 - m @ n.T/m_sq + m @ w.T/(w_sq*m_sq)*beta
-
-        elif w_sq <= threshold and m_sq > threshold and abs(beta) < threshold:
-            G = -v @ v.T @ pseudo_inv / v_sq - m @ n.T/m_sq
-
-        elif w_sq < threshold and abs(beta) > threshold:
-            t1 = v_sq/beta * m  + v
-            t2 = pseudo_inv.T @ v * m_sq/beta + n
-            coeff = beta/(v_sq*m_sq + beta**2)
-            G = m @ v.T @ pseudo_inv/beta - coeff * t1 @ t2.T
-
-        elif w_sq > threshold and m < threshold and abs(beta) < threshold:
-            G = -pseudo_inv @ n @ n.T/n_sq - v @ w.T/w_sq
-
-        elif m_sq < threshold and abs(beta) > threshold:
-            t1 = pseudo_inv @ n * w_sq/beta + v
-            t2 = w*n_sq/beta + n
-            coeff = beta/(n_sq*w_sq + beta**2)
-            G = pseudo_inv @ n @ w.T /beta - coeff * t1 @ t2.T
-
-        elif w_sq < threshold and m_sq < threshold and abs(beta) < threshold:
-            t1 = - v @ v.T @ pseudo_inv / v_sq
-            t2 = - pseudo_inv @ n @ n.T / n_sq
-            t3 = v.T @ pseudo_inv @ n @ v @ n.T / (v_sq * n_sq)
-            G = t1 + t2 + t3
-        else:
-            raise ValueError("Conditions on matrices lead to an invalid pseudoinverse")
-        
-        return new_A_sub, new_pseudo_inv + G
-
-    def r1_find_best_x(self, measurement, A_sub, pseudo_inv):
-        x = pseudo_inv @ measurement
-        resid = np.sum(np.square(measurement - A_sub @ x))/2
-        return x, resid
+        new_A = self.A[indices,:]
+        x_dict = np.linalg.lstsq(new_A.transpose(), measurement.transpose(), rcond=None)
+        return x_dict[0], x_dict[1].sum()/2
 
                 
     def choose_index(self, g):
@@ -216,8 +161,9 @@ if __name__ == "__main__":
     print("Y")
     print(Y.shape)
 
-    sg = SmoothGreedy(A.T, Y.T, data.T, k=20)
+    sg = SmoothGreedy(A, Y, data, k=20)  # M: either pass A.T and Y.T or change SmoothGreedy accordingly
     rmse = sg.evaluate()
     print("RMSE: ", rmse)
     np.savetxt("data/recovered_1", sg.reconstruction, delimiter=',')
 
+    
